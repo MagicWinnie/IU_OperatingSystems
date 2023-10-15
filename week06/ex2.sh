2d2ebbc8ea215ced8f61fff7bd87c2352b4fed02
MagicWinnie <magicwinnie@yandex.ru>
2023-10-15 16:23:49 +0300

    Added exercise 2 (without txt)

---
+++ week06/ex2.sh
@@ -0,0 +1,10 @@
+if [ $# -eq 0 ]
+    then
+        echo "Uh-oh! Path to the input file should be provided!"
+        exit 1
+fi
+
+gcc worker.c -o worker.out
+gcc scheduler.c -o scheduler.out
+
+./scheduler.out $1
