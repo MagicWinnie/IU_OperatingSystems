3fc2928730dce6b7fc48ce2fc472caf458237b27
MagicWinnie <magicwinnie@yandex.ru>
2023-10-15 19:59:25 +0300

    Added exercise 3

---
+++ week06/ex3.sh
@@ -0,0 +1,10 @@
+if [ $# -eq 0 ]
+    then
+        echo "Uh-oh! Path to the input file should be provided!"
+        exit 1
+fi
+
+gcc worker.c -o worker.out
+gcc scheduler_sjf.c -o scheduler_sjf.out
+
+./scheduler_sjf.out $1
