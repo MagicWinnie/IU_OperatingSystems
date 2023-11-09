01902ba4786612d3ec1220e628b6ea5b638ea429
MagicWinnie <magicwinnie@yandex.ru>
2023-11-09 19:11:35 +0300

    Added ex25

--- week10/ex25.sh
+++ week10/ex25.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
+echo "" > ex25/ex5.txt
 
+chmod a-w ex25/ex5.txt
+
+chmod uo+rwx ex25/ex5.txt
+
+chmod g=u ex25/ex5.txt
+
+# 660 means that owner and group users can read/write to this file,
+# while others do not have any access
+# -rw-rw----
+
+# 775 means that owner and group users can do everything to this file,
+# while others can read and execute
+# -rwxrwxr-x
+
+# 777 means that everybody can do everything to this file
+# -rwxrwxrwx
+
+rm ex25/ex5.txt
